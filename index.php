358532bccba559855c462577977ceec6e301f412
nair0lf32 <florianedem@gmail.com>
2021-09-16 18:15:54 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">  
+    <head>
+    <title></title>
+
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, user-scalable=no, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+
+    <link rel="shortcut icon" href="favicon.ico">
+
+    <link rel="stylesheet" href="/css/style.css">
+    <script src="/js/script.js"></script>
+</head>
+
+<body>
+    <header> 
+    <div class = container > 
+        <h1>PoSeid</h1>
+    <div class = eyeball>
+        <div class="inner-eye">
+            <div class="pupil"></div>
+            <div class="light"></div>
+            <div class="eyelid"></div>
+        </div>
+    </div>
+        <h1>n</h1>
+    </div>
+    </header>
+
+
+
+        <section class="content-main">
+
+        </section>
+
+        <footer>
+
+        </footer>
+    </div>
+
+
+
+
+</body>
+
+</html>
+
+
+<?php 
+
+
+
+
+echo "poseidon"
+
+
+?>
