2a3a97ee74f93ca98e2d86f1644783c952e9633f
nair0lf32 <florianedem@gmail.com>
2021-09-19 19:03:08 +0100

    minor fix

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ window.onload = function () {
     // for further animations
     const eye = document.querySelector('.eye-wrap');
     window.addEventListener('mousemove', (event) => {
-        const x = -(window.innerWidth / 2 - event.pageX) / 25;
-        const y = -(window.innerHeight / 2 - event.pageY) / 25;
+        const x = -(window.innerWidth / 2 - event.pageX) / 35;
+        const y = -(window.innerHeight / 2 - event.pageY) / 35;
         eye.style.transform = `rotate(-45deg) translateY(${y}px) translateX(${x}px)`;
     });
 
