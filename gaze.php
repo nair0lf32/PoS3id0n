359cb4e79725afac7ee4c97c5930dbe468a1be3a
nair0lf32 <florianedem@gmail.com>
2022-04-30 13:15:18 +0100

    minor updates

--- gaze.php
+++ gaze.php
@@ -23,9 +23,9 @@ if ($detector->isBot()) {
     $device = $detector->getDeviceName();
     $brand = $detector->getBrandName();
     $model = $detector->getModel();
-    echo "<p> Browser used: <span> {$client} </span>  
-installed on a <span> {$device} </span> with <span> {$os} </span> 
-<span> {$brand} </span> <span> {$model} </span>  </p>";
+    echo "<p> Browser used: <span> {$client} </span> <br> 
+Machine: <span> {$device} ,</span> <span> {$os} ,</span> 
+<span> {$brand} ,</span> <span> {$model} </span>  </p>";
 }
 
 //GET IP Address
@@ -47,44 +47,45 @@ function getIPAddress()
 }
 $IP_ADDRESS = getIPAddress();
 
-//Geolocation
+//Geolocation using ip-api
 $IP_API_URL = 'http://ip-api.com/json/' . urlencode($IP_ADDRESS) . '?fields=66846719';
 $response = file_get_contents($IP_API_URL);
 $response = json_decode($response, true);
 if (!empty($response)) {
     echo "<p> Location: <span> {$response['city']} {$response['country']} {$response['regionName']} {$response['continent']}</span>. <br>
     Timezone: <span>{$response['timezone']}</span>. <br> 
-    Precisely at: <span> latitude: {$response['lat']}, longitude: {$response['lon']}</span> with an
-    <span> accuracy radius of: {$response['offset']}</span>. </br> Common currency: <span> {$response['currency']} </span> </p> <br>";
+    Precisely at: <span> latitude: {$response['lat']}, longitude: {$response['lon']}</span> <br>
+    Accuracy radius: <span> {$response['offset']}</span>. </br> Common currency: <span> {$response['currency']} </span> </p> <br>";
 
     if ($response['mobile']) {
-        echo "<p> you are using a <span> mobile connection detected </span> or maybe a modem with a Sim-card...</p> <br>";
+        echo "<p> <span> mobile connection detected </span> or maybe a modem with a Sim-card...</p> <br>";
     }
-    echo "<p> ISP: <span> {$response['as']} with {$response['isp']} </span> </p> <br>";
+    echo "<p> ISP: <span> {$response['as']} {$response['isp']} </span> </p> <br>";
 
     if (!empty($_SERVER['HTTP_REFERER'])) {
-        echo "<p> hmm...Last referer: <span> {$_SERVER['HTTP_REFERER']} </span>? </p> <br>";
+        echo "<p> Last referer page: <span> {$_SERVER['HTTP_REFERER']} </span>? </p> <br>";
     }
 
     //Obfuscation Detection
     if ($response['proxy']) {
-        echo "<p> I sense obfuscation on your IP address you are trying to hide from 
-        the gaze with either a <i> vpn/proxy or Tor </i>...for legal reasons I have to ask: are you a criminal?
-        your Ip address is: <span> {$response['query']} </span>
-        I cannot see you through that without violating at least 32 laws...all the data above is biased
-        But I know it and I will get you someday (maybe when I own the internet) </p> <br>";
+        echo "<p> I sense obfuscation on your IP address. <br> you are trying to hide from 
+        the gaze with either a <i> vpn/proxy or Tor </i>. <br> For legal reasons I have to ask: are you a criminal? <br>
+        your Ip address is: <span> {$response['query']} </span> <br>
+        I cannot see you through that without violating at least 32 laws...<br>
+        therefore the data above might be biased </p>";
     } else {
-        echo "<p> No obfuscation detected: <i> no vpn </i>, <i> no proxy </i>, <i> no Tor</i>
-        you are not very challenging, nor original...here have your IP address <span> {$response['query']}</span> 
-        and go away 
-        </p> <br>";
+        echo "<p> No obfuscation detected: <br>
+        <i> no vpn </i>, <i> no proxy </i>, <i> no Tor</i> <br>
+        you are not very challenging, nor original... <br>
+        Your IP address is: <span> {$response['query']}</span> <br> 
+        </p>";
     }
 } else {
     echo "<p> I cannot Geolocate you..WHY CAN I NOT GEOLOCATE YOU? are you living under a rock? </p> <br>";
 }
 
 $today = new DateTime("now", new DateTimeZone($response['timezone']));
-echo "<p> Encounter date and time: <span> {$today->format('d-m-Y H:i:s')} </span>. Nothing special about this day! </p> <br>";
+echo "<p> Encounter date and time: <span> {$today->format('d-m-Y H:i:s')} </span> </p>";
 
 echo "<p> Interresting data... I could store it but it's not that important to me. <br>
 Be gone mortal! </p>";
