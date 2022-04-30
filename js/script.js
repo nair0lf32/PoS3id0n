6a6b9a43bf9928fb44d94366bc431518ff4b347e
nair0lf32 <florianedem@gmail.com>
2022-04-30 12:53:55 +0100

    update event handler

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 window.onload = function () {
 
     const eye = document.querySelector('.eye-wrap');
-    window.addEventListener('mousemove', (event) => {
+    window.addEventListener('pointermove', (event) => {
         const x = -(window.innerWidth / 2 - event.pageX) / 35;
         const y = -(window.innerHeight / 2 - event.pageY) / 35;
         eye.style.transform = `rotate(-45deg) translateY(${y}px) translateX(${x}px)`;
     });
 
     const light = document.querySelector('.light');
-    window.addEventListener('mousemove', (event) => {
+    window.addEventListener('pointermove', (event) => {
         const x2 = -(window.innerWidth / 2 - event.pageX) / 60;
         const y2 = -(window.innerHeight / 2 - event.pageY) / 60;
         light.style.transform = `rotate(-45deg) translateY(${y2}px) translateX(${x2}px)`;
